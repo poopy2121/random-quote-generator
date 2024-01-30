fd568fe302f1a03fc0d8d5eb77d4c8f4710fd8c3
poopy2121 <ggg1111g1g1g@gmail.com>
2024-01-30 21:50:27 +0100

    lets go

---
+++ script.js
@@ -0,0 +1,12 @@
+const btn = document.getElementById("inspire-btn");
+let quotesplace = document.getElementById("quotes")
+let quotes = ['My mother always used to say: The older you get, the better you get, unless you’re a banana.',
+             'Gentlemen, you cant fight in here. This is the war room',
+            'Before you criticize someone, you should walk a mile in their shoes. That way when you criticize them, you are a mile away from them and you have their shoes.']
+
+
+btn.addEventListener('click', function() {
+
+    
+})
+
